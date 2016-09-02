301249cd96297a7b571db9a116517fd41abfa6bc
pabloferro <pablo.pyp@gmail.com>
2016-09-02 15:10:13 -0300

    OAuth Callback

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function(req, res){
-    res.send('Pull Reviews');
+    res.send(`Pull Reviews<br>
+              <a href="https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${config.github.id}">
+                  Authorize PullReviews
+              </a>`);
 });
 
 const handleEvent = function(event, body) {
@@ -33,6 +36,11 @@ const handleEvent = function(event, body) {
 
 app.post('/event_handler', function (req, res) {
     handleEvent(req.headers['x-github-event'], req.body);
+    res.send('ok');
+});
+
+app.get('/oauth/callback', function(req, res) {
+    winston.info(`code: ${req.query.code}`);
     res.send('ok');
 });
 
